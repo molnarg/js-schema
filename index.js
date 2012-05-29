ee5bf4a89dd24930e2940fa48826bd82dc5f9c46
Gábor Molnár <gabor.molnar@sch.bme.hu>
2012-05-29 02:05:04 +0200

    Move includes to the index file.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 module.exports = require('./lib/schema')
+
+// Core schemas
+require('./lib/core/reference')
+
+require('./lib/core/nothing')
+require('./lib/core/anything')
+
+require('./lib/core/object')
+
+require('./lib/core/or')
+require('./lib/core/and')
+
+require('./lib/core/instanceof')
+require('./lib/core/schema')
