a289e6b9216af1d542e4ba64d4a5b0d63381b05f
Gábor Molnár <gabor.molnar@sch.bme.hu>
2012-06-14 16:36:40 +0200

    New pattern: deep equality.

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ module.exports = require('./lib/schema')
 
 // Core schemas
 require('./lib/patterns/reference')
-
 require('./lib/patterns/nothing')
 require('./lib/patterns/anything')
-
 require('./lib/patterns/object')
-
 require('./lib/patterns/or')
-
+require('./lib/patterns/equality')
+require('./lib/patterns/regexp')
 require('./lib/patterns/instanceof')
 require('./lib/patterns/schema')
-
-require('./lib/patterns/regexp')
 
 // Extensions
 require('./lib/extensions/Boolean')
