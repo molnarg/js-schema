b74542e622a65688ea7a64691486de34b139bbb0
parhelium <me@parhelium.pl>
2014-08-24 00:33:52 +0200

    feature errors : improved objects and visually improved tests failure message

--- test/extensions/errors-object.js
+++ test/extensions/errors-object.js
@@ -2,11 +2,17 @@ var vows = require('vows'),
     assert = require('assert'),
     schema = require('../../index.js');
 
+function showErrors(input, errors){
+    input = input || {};
+    return [
+        "","Input:",JSON.stringify(input, null, 2),
+        "","Errors:",JSON.stringify(errors, null,2)
+    ].join("\n")
+}
 // Create a Test Suite
 vows.describe('Validation Object with errors').addBatch({
     'Object': {
         'Invalid input : empty object':function(){
-
             var instance = schema({
                 func: Function,
                 number : Number.min(6).max(9),
@@ -14,16 +20,15 @@ vows.describe('Validation Object with errors').addBatch({
                 string : String
             });
             var result = instance.errors({});
-            console.log(JSON.stringify(result, null,2))
+            assert(!result,showErrors({}, result) )
 
         },
         'Invalid nested input':function(){
             var input = {
                 a : {
-                    aa:true,
-                    bb:1,
-                    cc:null
-                }
+                    aa:false
+                },
+                d:true
             };
 
             var aSchema = schema({
@@ -31,13 +36,14 @@ vows.describe('Validation Object with errors').addBatch({
                 bb:Boolean,
                 cc:Function
             })
-            var instance = schema({              // A duck
-                a : aSchema
+            var instance = schema({
+                a : aSchema,
+                b : Number,
+                c : Boolean,
+                d : Number
             });
             var result = instance.errors(input);
-
-            console.log(JSON.stringify(result, null,2));
-            assert(result);
+            assert(!result,showErrors(input, result))
         }
     }
 }).export(module)
