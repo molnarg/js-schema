10ecfa556865dda10dfd8be280c1a6919de432e1
parhelium <me@parhelium.pl>
2014-08-23 23:41:13 +0200

    tests : boolean

---
+++ test/extensions/errors-boolean.js
@@ -0,0 +1,24 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    schema = require('../../index.js');
+
+// Create a Test Suite
+vows.describe('Validation Boolean with errors').addBatch({
+    'Boolean': {
+        topic:function(){
+            return {
+                invalid_inputs:[0,-1,1,"false","true","whatever",NaN, [],{},/dwa/],
+            }
+        },
+        'invalid input -> [0,-1,1,"false","true","whatever",NaN, [],{},/dwa/]':function(topic){
+            topic.invalid_inputs.forEach(function(input){
+                var result = schema(Boolean).errors(input);
+                assert(
+                    /(.*) is not Boolean/.test(result.message),
+                    "Errors should throw : " + input + " is not Boolean"
+                )
+            })
+
+        }
+    }
+}).export(module)
