6c8b1cac7ee27423cdbb140016c3f59bacdfdd8f
parhelium <me@parhelium.pl>
2014-08-23 22:40:30 +0200

    tests : first real tests

--- test/extensions/errors-array.js
+++ test/extensions/errors-array.js
@@ -5,17 +5,22 @@ var vows = require('vows'),
 // Create a Test Suite
 vows.describe('Validation Array with errors').addBatch({
     'Array.of': {
-        topic: function () { return [9,0,1,2,3,4,5,6]},
-        'Number.min(x}.max(y)':{
-            'One number is too big': function (topic) {
-                var aSchema = schema(Array.of(Number.min(0).max(5)));
-                var result = aSchema.errors(topic);
-
-                console.log(JSON.stringify(result, null,2));
-                assert
-            }
+        'Number -> input : [null, undefined, Object, Function]':function(){
+            var input = [
+                null, undefined, Object, Function
+            ];
+            var aSchema = schema(Array.of(Number));
+            input.forEach(function(input){
+                var result = aSchema.errors(input);
+                assert(/(.*) is not an instance of Array/.test(result.message), "Errors should return  'X is not an instance of Array'");
+            })
+        },
+        'Number -> input : only numbers': function () {
+                var input = [9,0,1,2,3,4,5,6];
+                var aSchema = schema(Array.of(Number));
+                var result = aSchema.errors(input);
+                assert(result === false, "errors method should return false");
         }
-
     },
     'Array.of(length, pattern)': {},
     'Array.of(minLength, maxLength, pattern)': {},
