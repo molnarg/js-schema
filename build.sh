721174f2d97a1cbcdd87ff2132a680b24bec7ca1
parhelium <me@parhelium.pl>
2014-08-27 23:08:59 +0200

    Variables definitions are aligned and space between function and () is removed.

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 DEBUG="js-schema.debug.js"
 MIN="js-schema.min.js"
 
-browserify index.js | sed 's|require("/index.js")|window.schema = require("/index.js")|g' >$DEBUG
 
+browserify index.js | sed 's|require("/index.js")|window.schema = require("/index.js")|g' > $DEBUG
+browserify index.js > "example/$DEBUG"
 uglifyjs $DEBUG >$MIN
+uglifyjs "example/$DEBUG" > "example/$MIN"
