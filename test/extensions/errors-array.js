5ac398fd8f7dc542d4e136e8506d61367920b669
parhelium <me@parhelium.pl>
2014-08-27 16:34:29 +0200

    stylistic fix : applied jsbeautifier with indent with 2 spaces

--- test/extensions/errors-array.js
+++ test/extensions/errors-array.js
@@ -1,29 +1,29 @@
 var vows = require('vows'),
-    assert = require('assert'),
-    printTestResult = require('../printTestResult.js'),
-    schema = require('../../index.js');
+  assert = require('assert'),
+  printTestResult = require('../printTestResult.js'),
+  schema = require('../../index.js');
 
 // Create a Test Suite
 vows.describe('Validation Array with errors').addBatch({
-    'Array.of': {
-        'Number -> invalid input : [null, undefined, Object, Function]':function(){
-            var input = [
-                null, undefined, Object, Function
-            ];
-            var aSchema = schema(Array.of(Number));
-            input.forEach(function(input){
-                var result = aSchema.errors(input);
-                assert(/(.*) is not an instance of Array/.test(result), "Errors should return  '"+input+" is not an instance of Array'");
-            })
-        },
-        'Number -> valid input : only numbers': function () {
-                var input = [9,0,1,2,3,4,5,6];
-                var aSchema = schema(Array.of(Number));
-                var result = aSchema.errors(input);
-                assert(result === false, "errors method should return false");
-        }
+  'Array.of': {
+    'Number -> invalid input : [null, undefined, Object, Function]': function() {
+      var input = [
+        null, undefined, Object, Function
+      ];
+      var aSchema = schema(Array.of(Number));
+      input.forEach(function(input) {
+        var result = aSchema.errors(input);
+        assert(/(.*) is not an instance of Array/.test(result), "Errors should return  '" + input + " is not an instance of Array'");
+      })
     },
-    'Array.of(length, pattern)': {},
-    'Array.of(minLength, maxLength, pattern)': {},
-    'Array.like(array)': {}
+    'Number -> valid input : only numbers': function() {
+      var input = [9, 0, 1, 2, 3, 4, 5, 6];
+      var aSchema = schema(Array.of(Number));
+      var result = aSchema.errors(input);
+      assert(result === false, "errors method should return false");
+    }
+  },
+  'Array.of(length, pattern)': {},
+  'Array.of(minLength, maxLength, pattern)': {},
+  'Array.like(array)': {}
 }).export(module)
--- test/extensions/errors-object.js
+++ test/extensions/errors-object.js
@@ -1,55 +1,58 @@
 var vows = require('vows'),
-    assert = require('assert'),
-    printTestResult = require('../printTestResult.js'),
-    schema = require('../../index.js');
+  assert = require('assert'),
+  printTestResult = require('../printTestResult.js'),
+  schema = require('../../index.js');
 
 
 // Create a Test Suite
 vows.describe('Validation Object with errors').addBatch({
-    'Object': {
-        'Invalid input : empty object':function(){
-            var input = {object:{}, number:3};
-            var B = function B(){};
-            var instance = schema({
-                func    : Function,
-                number  : Number.min(6).max(9),
-                boolean : Boolean,
-                string  : String,
-                class  : B,
-                object: {
-                    a:Number.min(2).max(7),
-                    b:Function
-                }
-            });
-            var result = instance.errors(input);
-            assert(result == false, printTestResult(input, result));
-        },
-        'Invalid nested input':function(){
-            var input = {
-                a : {
-                    aa:false,
-                    cc:false
-                },
-                d:true
-            };
-            var instance = schema({
-                a : {
-                    aa:[Number.min(2), String],
-                    bb:{
-                        aaa:Boolean,
-                        bbb:Number,
-                        ccc:{
-                            aaaa:Boolean
-                        }
-                    },
-                    cc:Function
-                },
-                b : Number,
-                c : Boolean,
-                d : Number
-            });
-            var result = instance.errors(input);
-            assert(result == false,printTestResult(input, result))
+  'Object': {
+    'Invalid input : empty object': function() {
+      var input = {
+        object: {},
+        number: 3
+      };
+      var B = function B() {};
+      var instance = schema({
+        func: Function,
+        number: Number.min(6).max(9),
+        boolean: Boolean,
+        string: String,
+        class: B,
+        object: {
+          a: Number.min(2).max(7),
+          b: Function
         }
+      });
+      var result = instance.errors(input);
+      assert(result == false, printTestResult(input, result));
+    },
+    'Invalid nested input': function() {
+      var input = {
+        a: {
+          aa: false,
+          cc: false
+        },
+        d: true
+      };
+      var instance = schema({
+        a: {
+          aa: [Number.min(2), String],
+          bb: {
+            aaa: Boolean,
+            bbb: Number,
+            ccc: {
+              aaaa: Boolean
+            }
+          },
+          cc: Function
+        },
+        b: Number,
+        c: Boolean,
+        d: Number
+      });
+      var result = instance.errors(input);
+      assert(result == false, printTestResult(input, result))
     }
+  }
 }).export(module)
