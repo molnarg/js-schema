8f0039d6d6ef1a6e9c19cf7c4cbd857a91c282c7
parhelium <me@parhelium.pl>
2014-08-29 13:17:29 +0200

    Removed BaseSchema.err function

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ MIN="js-schema.min.js"
 
 
 browserify index.js | sed 's|require("/index.js")|window.schema = require("/index.js")|g' > $DEBUG
+browserify index.js > "example/$DEBUG"
 uglifyjs $DEBUG >$MIN
+uglifyjs "example/$DEBUG" > "example/$MIN"
