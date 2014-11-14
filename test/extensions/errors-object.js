1a7909a566666c01ae0c73b7539a90e291768feb
parhelium <me@parhelium.pl>
2014-11-14 12:05:26 +0100

    tests : rewrote errors-object

--- test/extensions/errors-object.js
+++ test/extensions/errors-object.js
@@ -1,62 +1,36 @@
 var vows            = require('vows')
   , assert          = require('assert')
-  , printTestResult = require('../printTestResult.js')
   , schema          = require('../../index.js')
 
 
 // Create a Test Suite
-vows.describe('Validation Object with errors').addBatch({
-  'Object': {
-    'Invalid input : empty object': function() {
-      var input = {
-        object: {},
-        number: 3
-      };
-      var B = function B() {};
-      var instance = schema({
-        func: Function,
-        number: Number.min(6).max(9),
-        boolean: Boolean,
-        string: String,
-        class: B,
-        object: {
-          a: Number.min(2).max(7),
-          b: Function
+vows.describe('Testing if errors() precisely informs about cause').addBatch({
+
+    'Number':{
+        topic:{
+            schema                  : schema({test:Number.min(0).max(10)}),
+            input_invalid_string    : {test: "hello world"},
+            input_invalid_number    : {test: 190},
+            input_valid             : {test: 5}
+        },
+        'Object { test : Number }  : String was passed instead Number':function(t){
+            var validation = false === t.schema(t.input_invalid_string); 
+            assert(validation, 'Validation is broken. Incorrect object should NOT be validated.');
+
+            var errors = t.schema.errors(t.input_invalid_string);
+            validation = errors.test == 'dwadawd';
+            
+            assert(validation, 'Function schema.errors returns incorrect message : \n \t\t'+ vows.inspect(errors));
+        },
+        'Object { test : Number }  : Too big Number was passed ':function(t){
+            var validation = false === t.schema(t.input_invalid_number);
+            assert(validation, 'Validation is broken. Incorrect object should NOT be validated.');
+
+            var errors = t.schema.errors(t.input_invalid_number);
+            validation = errors.test == 'dwadawd';
+
+            assert(validation, 'Function schema.errors returns incorrect message : \n \t\t'+ vows.inspect(errors));
         }
-      });
-      var result = instance.errors(input);
-      console.log('Input : \n', vows.inspect(input));
-      console.log('Errors : \n', vows.inspect(result));
-      assert(result, printTestResult(input, result))
-    },
-    'Invalid nested input': function() {
-      var input = {
-        a: {
-          aa: false,
-          cc: false
-        },
-        d: true
-      };
-      var instance = schema({
-        a: {
-          aa: [Number.min(2), String],
-          bb: {
-            aaa: Boolean,
-            bbb: Number,
-            ccc: {
-              aaaa: Boolean
-            }
-          },
-          cc: Function
-        },
-        b: Number,
-        c: Boolean,
-        d: Number
-      });
-      var result = instance.errors(input);
-      console.log('Input : \n', vows.inspect(input));
-      console.log('Errors : \n', vows.inspect(result));
-      assert(result, printTestResult(input, result))
     }
-  }
+
 }).export(module)
